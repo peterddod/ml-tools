2663882a8d911488e24d22d921167498a51c5809
Peter Dodd <peterdodd@glaroam2-158-239.wireless.gla.ac.uk>
2022-12-19 17:34:59 +0000

    added reset seed function to utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import torch
+import random
+import numpy as np
 
 
 def label_vectoriser(labels, classes, label_smoothing=0.1):
@@ -15,3 +17,8 @@ def label_vectoriser(labels, classes, label_smoothing=0.1):
             y[i,j] = (1-label_smoothing)*onehot + label_smoothing/classes
 
     return y
+
+def resetseed(seed=0):
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
