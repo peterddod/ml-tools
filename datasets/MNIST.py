7cbc9f3e60c05cc287c2363bae397255fb085e71
Peter Dodd <peterdodd@glaroam2-158-239.wireless.gla.ac.uk>
2022-12-20 16:55:09 +0000

    updated parts

--- datasets/MNIST.py
+++ datasets/MNIST.py
@@ -1,29 +1,26 @@
 import torch
+from torch.nn import functional as F
 from torch.utils.data import Dataset
 from keras.datasets import mnist
 from ..utils import label_vectoriser
 
 
 class MNIST(Dataset):
-    def __init__(self, train=True, scaled=False, augment=False, label_smoothing=0.0):
+    def __init__(self, train=True, one_hot_labels=True):
+        self.one_hot_labels = one_hot_labels
+
         (train_X, train_y), (test_X, test_y) = mnist.load_data()
-
-        train_X = torch.reshape(torch.from_numpy(train_X), (-1, 1, 28, 28)).float()
-        test_X = torch.reshape(torch.from_numpy(test_X), (-1, 1, 28, 28)).float()
-
-        train_y = torch.from_numpy(train_y).float()
-        test_y = torch.from_numpy(test_y).float()
 
         if train:
             self.data =  {
-                'X': train_X, 
-                'y': train_y, 
+                'X': torch.reshape(torch.from_numpy(train_X), (-1, 1, 28, 28)), 
+                'y': torch.from_numpy(train_y), 
             }
         else:
             self.data =  {
-            'X': test_X, 
-            'y': test_y, 
-        }
+                'X': torch.reshape(torch.from_numpy(test_X), (-1, 1, 28, 28)), 
+                'y': torch.from_numpy(test_y), 
+            }
 
     def __len__(self):
         return len(self.data['X'])
@@ -32,6 +29,18 @@ class MNIST(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        sample = (self.data['X'][idx], self.data['y'][idx])
+        X = self.data['X'][idx]
+        y = self.data['y'][idx]
 
-        return sample
+        if self.one_hot_labels:
+            y = F.one_hot(y.to(torch.int64), 10)
+
+        X, y = X.float() ,y.float()
+
+        return (X,y)
+
+    def train(self):
+        self.one_hot_labels = True
+
+    def eval(self):
+        self.one_hot_labels = False
